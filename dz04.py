80476f7ad9fc2424d84f9172fcdd2581a0ea8d82
Ascetholix <sangak@mail.ru>
2022-11-27 23:30:29 +0300

    corrections dz04.py

--- dz04.py
+++ dz04.py
@@ -10,19 +10,24 @@
 n = int(input("Введите число: "))
 
 list = []
-
+b = True
+result =0
 for i in range(-n , n+1):  
   list.append(i)
 
 print(list)
   
 index = input("Укажите индексы: ")
-compos = index.split(' ')           # методом split делим индекс 
+compos = index.split(' ')        # методом split делим индекс 
+
+print(compos)
 
 for j in compos[1:]:             # начинаем от 2 позиции
-  compos[0] = int(compos[0])         # перевдоим на int первую позицию
+  if b == True:                    # буливая перемная для первичного присвоения
+    result = int(list[0])         # перевдоим на int первую позицию
+    b = False
   j=int(j)                           # перевдоим на int индексы
-  result = list[compos[0]] * list[j]
+  result = result * list[j]
 
 print(result , end= ' ')
 
